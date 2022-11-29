f9da75dc6f4e0d5c62b014ee30c928a42236eac4
Ivan Robles <irae45@gmail.com>
2022-11-28 23:38:56 -0600

    Add webpack and eslint config.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+const App = () => (
+  <h1>
+    Welcome to Sharmaz Portfolio/Resume
+  </h1>
+);
+
+export default App;
+
+reactDom.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: "./src/index.html",
+ filename: "index.html"
+});
+
+module.exports={
+    mode: "development", 
+    entry: "./src/index.js", 
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    target: "web",
+    devServer: {
+        port: "5000",
+        static: ["./dist"],
+        open: true,
+        hot: true ,
+        liveReload: true
+    },
+    resolve: {
+        extensions: ['.js','.jsx','.json'] 
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:  'babel-loader'
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+}
