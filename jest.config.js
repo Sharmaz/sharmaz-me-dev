ec234515a3858d1a4061ad55f6376bf40a5fb43b
Ivan Robles <irae45@gmail.com>
2022-12-01 01:58:57 -0600

    add jest workflow and fix jest config.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__tests__/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/src/__tests__/__mocks__/styleMock.js',
+  },
 };
