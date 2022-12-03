3ed9f56a1e5bf99ef36ee541aff8f3f8fe2ac1d8
Ivan Robles <irae45@gmail.com>
2022-12-02 19:41:04 -0600

    fix eslint script for jsx extension and a proptype lint error.

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import './layout.css';
@@ -9,5 +10,9 @@ const Layout = ({ children }) => (
     <Footer />
   </div>
 );
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default Layout;
