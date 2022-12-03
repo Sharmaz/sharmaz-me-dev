a5cf5f3d1c00cad25b135031fc0c85dfdfbd2cec
Ivan Robles <irae45@gmail.com>
2022-12-02 19:06:30 -0600

    add basic layout and enable jsx runtime.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Layout from './layout/Layout';
+import './app.css';
+
+const App = () => (
+  <Layout>
+    <h1 className="text-primary font-bold">
+      Welcome to Sharmaz Portfolio/Resume
+    </h1>
+  </Layout>
+);
+
+export default App;
