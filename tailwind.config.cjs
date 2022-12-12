0b155b3da8914b0531ef1ebd40f43c12cf7da01f
Ivan Robles <irae45@gmail.com>
2022-12-12 17:12:16 -0600

    add button gradient animation.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,16 @@ module.exports = {
         'dark-orchid': '#8233C5',
         'metallic-blue': '#274B74',
       },
+      animation: {
+        'purple-animation': 'animationGradient 2s ease alternate infinite',
+      },
+      keyframes: {
+        animationGradient: {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '50% 100%' },
+          '100%': { 'background-position': '100% 0%' },
+        },
+      },
     },
   },
   plugins: [],
