bfbeeded0b4535146cad0803e441bfdc6410500a
Ivan Robles <irae45@gmail.com>
2022-12-17 01:40:31 -0600

    add mobile nav animation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
 
   return (
     <header className="fixed w-full top-0 z-20 backdrop-blur">
-      <nav className="flex items-center justify-evenly  py-2 relative max-w-screen-lg mx-auto">
-        <a href="/">
+      <nav className="flex items-center justify-between md:justify-evenly py-2 relative max-w-screen-lg mx-auto">
+        <a href="/" className="ml-4 md:ml-0">
           <div className="flex items-center">
             <img src={logo} alt="Ivan Robles" width={48} />
             <h3 className="text-4xl md:text-2xl ml-2">Ivan Robles</h3>
@@ -21,7 +21,7 @@ const Header = () => {
         <div
           role="button"
           aria-hidden="true"
-          className="menu-icon w-10 h-10 md:hidden"
+          className="menu-icon w-10 h-10 md:hidden mr-4"
           onClick={showMenu}
         >
           <img src={show ? CloseMenuIcon : MenuIcon} alt="menu icon" />
@@ -43,31 +43,27 @@ const Header = () => {
         <div className="button-purple hidden md:block  h-10 w-[132px]">
           <button type="button" className="hidden bg-background md:block w-32  p-1 text-xl rounded-full m-0.5">Resume</button>
         </div>
-        <div className="bg-gradient-to-tl from-heliotrope via-dark-orchid to-metallic-blue w-full h-0.5 absolute bottom-0 left-0" />
-        {
-          show ? (
-            <nav className="absolute w-full h-screen bg-background top-16">
-              <ul className="flex flex-col text-2xl ml-4">
-                <li className="my-8">
-                  <a href="#about" className="no-underline text-slate-400">About me</a>
-                </li>
-                <li className="my-8">
-                  <a href="#experience" className="no-underline text-slate-400 my-8">Experience</a>
-                </li>
-                <li className="my-8">
-                  <a href="#work" className="no-underline text-slate-400 my-8">Work</a>
-                </li>
-                <li className="my-8">
-                  <a href="#contact" className="no-underline text-slate-400 my-8">Contact me</a>
-                </li>
-              </ul>
-              <div className="button-purple ml-4 mt-4 w-[248px]">
-                <button type="button" className="bg-background p-2 text-2xl rounded-full ml-1 my-1 w-60">Resume</button>
-              </div>
-            </nav>
-          )
-            : null
-        }
+        <div className="purple-gradient w-full h-0.5 absolute bottom-0 left-0" />
+
+        <nav className={`absolute w-full h-screen bg-background top-16 transition-opacity ease-in duration-300 ${!show ? 'opacity-0 translate-y-full ' : 'opacity-100 translate-y-0'}`}>
+          <ul className="flex flex-col text-2xl ml-4">
+            <li className="my-8">
+              <a href="#about" onClick={showMenu}>About me</a>
+            </li>
+            <li className="my-8">
+              <a href="#experience" onClick={showMenu}>Experience</a>
+            </li>
+            <li className="my-8">
+              <a href="#work" onClick={showMenu}>Work</a>
+            </li>
+            <li className="my-8">
+              <a href="#contact" onClick={showMenu}>Contact me</a>
+            </li>
+          </ul>
+          <div className="button-purple ml-4 mt-4 w-[248px]">
+            <button type="button" className="bg-background p-2 text-2xl rounded-full ml-1 my-1 w-60">Resume</button>
+          </div>
+        </nav>
       </nav>
     </header>
   );
