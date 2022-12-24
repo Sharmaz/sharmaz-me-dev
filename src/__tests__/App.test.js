77a16d58392d860b97c3ccb0ef5f6970221ab77a
Ivan Robles <irae45@gmail.com>
2022-12-24 00:08:10 -0600

    fix test error due to intersection observer.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,12 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 
+const intersectionObserverMock = () => ({
+  observe: () => null,
+});
+
+window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
+
 describe('App tests', () => {
   it('should contains the heading 1', () => {
     render(<App />);
