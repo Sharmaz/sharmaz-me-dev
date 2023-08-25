133c4505c2194151ea9c1162009bd020f1a6cd98
Ivan Robles <irae45@gmail.com>
2023-08-25 14:29:51 -0600

    change mocks to ts and tsx for testing purposes.

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,8 @@ module.exports = {
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__tests__/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/src/__tests__/__mocks__/styleMock.js',
-    '\\.svg': '<rootDir>/src/__tests__/__mocks__/svg.js',
+      '<rootDir>/src/__tests__/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/src/__tests__/__mocks__/styleMock.ts',
+    '\\.svg': '<rootDir>/src/__tests__/__mocks__/svg.tsx',
   },
 };
