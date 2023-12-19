a172d1121d02c7646e927157f54ba6aace4f32d5
Ivan Robles <irae45@gmail.com>
2023-12-18 22:26:17 -0600

    add text animation at hero.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,9 +16,9 @@ const Hero = ({ name, resumeLink }: HeroProps) => {
 
   return (
     <section ref={ref} className="text-2xl text-slate-300 mx-4 mb-10 mt-36 md:text-5xl max-w-screen-lg md:pl-8 md:mx-auto relative">
-      <span className="block">Hi,</span>
-      <span className="block">{`I’m ${name},`}</span>
-      <span className="block">a Frontend Developer</span>
+      <span className="block relative p-[2px_0] hero-style before:content-['Hi,'] before:orange-gradient before:animation-delay-0">Hi,</span>
+      <span className="block relative p-[2px_0] hero-style before:content-['I’m_Ivan_Robles,'] before:red-gradient before:animation-delay-3">{`I’m ${name},`}</span>
+      <span className="block relative p-[2px_0] hero-style before:content-['a_Frontend_Developer'] before:purple-gradient before:animation-delay-6">a Frontend Developer</span>
       <div className="button-purple mt-8 md:mt-16 w-[248px] h-14 flex align-middle">
         <a
           className="bg-background p-2 text-2xl rounded-full mx-1 my-1 w-60 text-center"
